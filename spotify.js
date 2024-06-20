8e009515aac29e5bee595347ff94f81a0e9f574d
snigdhaydv27 <snigdhak.ug23.cs@nitp.ac.in>
2024-06-20 13:55:51 +0530

    seekbar and playbutton are clearly functionable now

--- spotify.js
+++ spotify.js
@@ -1,30 +1,54 @@
 let currentSong = new Audio();
-async function getSongs(){
-let a = await fetch("http://127.0.0.1:5500/Spotifyclone/songs/")
-let response = await a.text();
-console.log(response)
-let div = document.createElement("div")
-div.innerHTML = response;
-let as = div.getElementsByTagName("a")
-let songs = []
-for(let index = 0; index < as.length; index++){
+
+function secondsToMinutesSeconds(seconds) {
+  var minutes = Math.floor(seconds / 60);
+  var remainingSeconds = seconds % 60;
+  return (
+    minutes +
+    ":" +
+    (remainingSeconds < 10 ? "0" : "") +
+    Math.floor(remainingSeconds)
+  );
+}
+
+async function getSongs() {
+  let a = await fetch("http://127.0.0.1:5500/Spotifyclone/songs/");
+  let response = await a.text();
+  console.log(response);
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+  let songs = [];
+  for (let index = 0; index < as.length; index++) {
     const element = as[index];
-    if(element.href.endsWith(".mp3")){
-        songs.push(element.href.split("/songs/")[1])
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href.split("/songs/")[1]);
     }
+  }
+  return songs;
 }
- return songs 
-}
-const playMusic = (track)=>{
-    // let audio = new Audio("/Spotifyclone/songs/" + track)
-    currentSong.src = "/Spotifyclone/songs/" + track
-    currentSong.play()
-}
-async function main(){
-    let songs = await getSongs();
-    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
-    for (const song of songs){
-        songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" src="music.svg" alt="">
+
+const playMusic = (track, pause=false) => {
+  // let audio = new Audio("/Spotifyclone/songs/" + track)
+  currentSong.src = "/Spotifyclone/songs/" + track;
+  if(!pause){
+    currentSong.play();
+    play.src = "pause.svg";
+  }
+  document.querySelector(".songinfo").innerHTML = decodeURI(track);
+  document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
+};
+
+async function main() {
+  let songs = await getSongs();
+  playMusic(songs[0], true)
+  let songUL = document
+    .querySelector(".songList")
+    .getElementsByTagName("ul")[0];
+  for (const song of songs) {
+    songUL.innerHTML =
+      songUL.innerHTML +
+      `<li><img class="invert" src="music.svg" alt="">
               <div class="info">
                 <div> ${song.replaceAll("%20", " ")} </div>
                 <div>Snigdha</div>
@@ -34,12 +58,39 @@ async function main(){
                 <img src="play.svg" alt="">
               </div>
              </li>`;
+  }
+
+  Array.from(
+    document.querySelector(".songList").getElementsByTagName("li")
+  ).forEach((e) => {
+    e.addEventListener("click", (element) => {
+      console.log(e.querySelector(".info").firstElementChild.innerHTML);
+      playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
+    });
+  });
+
+  play.addEventListener("click", () => {
+    if (currentSong.paused) {
+      currentSong.play();
+      play.src = "pause.svg";
+    } else {
+      currentSong.pause();
+      play.src = "play.svg";
     }
-    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
-        e.addEventListener("click", element=>{
-            console.log(e.querySelector(".info").firstElementChild.innerHTML)
-        playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
-        })  
-     })
+  });
+
+  currentSong.addEventListener("timeupdate", () => {
+    document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(
+      currentSong.currentTime
+    )}/${secondsToMinutesSeconds(currentSong.duration)}`;
+    document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
+  });
+
+ document.querySelector(".seekbar").addEventListener("click", e=>{
+    let percent = (e.offsetX/e.target.getBoundingClientRect().width) * 100;
+    document.querySelector(".circle").style.left = percent + "%";
+    currentSong.currentTime = ((currentSong.duration)*percent)/100
+ })
+
 }
-main()
+main();
