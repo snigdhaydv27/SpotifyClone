c6b41d66c760b12ebb11f00fe8bda3b5dd1894be
snigdhaydv27 <snigdhak.ug23.cs@nitp.ac.in>
2024-06-20 15:01:37 +0530

    somewhat website is responsive

--- spotify.js
+++ spotify.js
@@ -91,6 +91,14 @@ async function main() {
     document.querySelector(".circle").style.left = percent + "%";
     currentSong.currentTime = ((currentSong.duration)*percent)/100
  })
+  
+   document.querySelector(".hamburger").addEventListener("click", ()=>{
+      document.querySelector(".left").style.left ="0"
+   })
+ 
+   document.querySelector(".close").addEventListener("click", ()=>{
+    document.querySelector(".left").style.left ="-120%"
+ })
 
 }
 main();
