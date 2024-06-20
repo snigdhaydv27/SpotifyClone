5841276bf1f0ac782e83bfa312c48f160e9e64d4
snigdhaydv27 <snigdhak.ug23.cs@nitp.ac.in>
2024-06-20 09:48:00 +0530

    playlist modified

--- spotify.js
+++ spotify.js
@@ -19,7 +19,16 @@ async function main(){
     let songs = await getSongs();
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
     for (const song of songs){
-        songUL.innerHTML = songUL.innerHTML + `<li> ${song.replaceAll("%20", " ")} </li>`;
+        songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" src="music.svg" alt="">
+              <div class="info">
+                <div> ${song.replaceAll("%20", " ")} </div>
+                <div>Snigdha</div>
+              </div>
+              <div class="playnow">
+                <span>Play Now</span>
+                <img src="play.svg" alt="">
+              </div>
+             </li>`;
     }
     var audio = new Audio(songs[0]);
     audio.play();
